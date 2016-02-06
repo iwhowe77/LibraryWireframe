c35fb432548b09438a949d3a702056a5e9d11b1f
ihowe6189 <ihowe6189@stu.d214.org>
2016-02-05 22:59:17 -0600

    setup login
    -next step would be to setup segue

---
+++ LibraryWireframe/LibraryWireframe/LoginController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LibraryWireframe
+//
+//  Created by Ian Howe on 2/5/16.
+//  Copyright © 2016 Ian Howe. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController{
+
+    @IBOutlet weak var loginView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginView.layer.shadowOpacity = 0.5
+        
+    }
+
+}
+
---
+++ LibraryWireframe/LibraryWireframe/WebViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WebViewController.swift
+//  LibraryWireframe
+//
+//  Created by Bill Howe on 2/5/16.
+//  Copyright © 2016 Ian Howe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WebViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
