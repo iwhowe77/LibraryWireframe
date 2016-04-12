3832310f8db38eb40f23fd723d9af41cc6c32248
ihowe6189 <ihowe6189@stu.d214.org>
2016-04-12 17:06:36 -0500

    v0.2
    Setup working table & browser
    -Bug: issue with random cells turning green

---
+++ LibraryWireframe/LibraryWireframe/UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor.swift
+//  LibraryWireframe
+//
+//  Created by Bill Howe on 4/12/16.
+//  Copyright © 2016 Ian Howe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIColor {
+    class func paleGreenColor() -> UIColor {
+        return UIColor(red: 102.0/255.0, green: 204.0/255.0, blue: 153.0/255.0, alpha: 1.0)
+    }
+}
--- LibraryWireframe/LibraryWireframe/WebViewController.swift
+++ LibraryWireframe/LibraryWireframe/WebViewController.swift
@@ -19,11 +19,104 @@ class WebViewController: UIViewController, UITableViewDataSource, UITableViewDel
     
     required init?(coder aDecoder: NSCoder)
     {
+        let cellTitles = [
+            ["Research",
+                "GALE PowerSearch",
+                "Biography in Context",
+                "Global Issues in Context",
+                "InfoTrac",
+                "LitFinder",
+                "Opposing Viewpoints in Context",
+                "Science in Context",
+                "Student Resource in Context",
+                "Consumer Reports",
+                "CQ Researcher",
+                "EBSCO",
+                "MAS-ULTRA (in school use)",
+                "Health Source",
+                "SIRS",
+                "Teen Health & Wellness",
+                "World Book"],
+            ["Social Science",
+                "ABC-CLIO (multi-databases)",
+                "American Government",
+                "American History",
+                "CountryReports",
+                "Daily Life Through History",
+                "Geography (U.S.)",
+                "Geography (World)",
+                "World History: Ancient",
+                "World History: Modern"],
+            ["eBooks & eAudio",
+                "OverDrive",
+                "Gale Virtual Reference",
+                "Gale Virtual Reference (PHS)",
+                "Flipster - BGHS",
+                "Flipster - EGHS",
+                "Flipster - JHHS",
+                "Flipster - PHS",
+                "Flipster - RMHS",
+                "Flipster - WHS"],
+            ["Autos & Careers",
+                "All Data Pro (Auto)",
+                "Career Cruising"]]
+
+        let cellLinks = [["http://www.d214.org/academics/district-library-resources/#Research",
+            "http://go.galegroup.com/ps/i.do?v=2.1&u=dist214&pg=BasicSearch&it=static&sw=w&p=GPS",
+            "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=BIC1",
+            "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=GIC",
+            "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=STOM",
+            "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=LITFS",
+            "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=OVIC",
+            "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=SCIC",
+            "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=SUIC",
+            "http://ezproxy.d214.org:2048/login?url=http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=crcro",
+            "http://ezproxy.d214.org:2048/login?url=http://library.cqpress.com/cqres/lpext.dll?f=templates&fn=titlepage.htm",
+            "http://ezproxy.d214.org:2048/login?url=http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=ehost",
+            "http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=ehost&defaultdb=ulh",
+            "http://ezproxy.d214.org:2048/login?url=http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=ehost&defaultdb=hxh",
+            "http://ezproxy.d214.org:2048/login?url=http://sks.sirs.com/",
+            "http://ezproxy.d214.org:2048/login?url=http://www.teenhealthandwellness.com/",
+            "http://ezproxy.d214.org:2048/login?url=http://www.worldbookonline.com/?subacct=N2771"],
+            ["http://www.d214.org/academics/district-library-resources/#social",
+                "http://ezproxy.d214.org:2048/login?url=http://www.databases.abc-clio.com",
+                "http://ezproxy.d214.org:2048/login?url=http://americangovernment.abc-clio.com/",
+                "http://ezproxy.d214.org:2048/login?url=http://americanhistory.abc-clio.com/",
+                "http://ezproxy.d214.org:2048/login?url=http://www.countryreports.org/",
+                "http://ezproxy.d214.org:2048/login?url=http://dailylife.abc-clio.com/",
+                "http://ezproxy.d214.org:2048/login?url=http://usgeography.abc-clio.com/",
+                "http://ezproxy.d214.org:2048/login?url=http://worldgeography.abc-clio.com/",
+                "http://ezproxy.d214.org:2048/login?url=http://ancienthistory.abc-clio.com/",
+                "http://ezproxy.d214.org:2048/login?url=http://worldhistory.abc-clio.com/"],
+            ["http://www.d214.org/academics/district-library-resources/#eBooks",
+                "http://ezproxy.d214.org:2048/login?url=http://d214.lib.overdrive.com/",
+                "http://infotrac.galegroup.com/itweb/dist214?db=GVRL",
+                "http://galepages.com/prospecths",
+                "http://search.ebscohost.com/login.aspx?authtype=ip,uid&custID=s9461948&profile=eon",
+                "http://search.ebscohost.com/login.aspx?authtype=ip,uid&group=main&custid=s9461831",
+                "http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=eon&custid=s9461855",
+                "http://search.ebscohost.com/login.aspx?authtype=ip,uid&custid=s9461881&profile=eon",
+                "http://search.ebscohost.com/login.aspx?authtype=ip,uid&custid=s9461905&profile=eon",
+                "http://search.ebscohost.com/login.aspx?authtype=ip,uid&custid=s9461926&profile=eon"],
+            ["http://www.d214.org/academics/district-library-resources/#Auto",
+                "http://ezproxy.d214.org:2048/login?url=http://library.alldatapro.com",
+                "http://ezproxy.d214.org:2048/login?url=http://www.careercruising.com/home/GroupLogin.aspx"]]
+        
         cellData = [TableViewItem]()
         for var i = 0; i < cellTitles.count; i++ {
-            let newItem = TableViewItem(LabelName: cellTitles[i], Link: cellLinks[i])
-            cellData.append(newItem)
+            for var j = 0; j < cellTitles[i].count; j++ {
+                if j == 0 {
+                    let newItem = TableViewItem(LabelName: cellTitles[i][j], Link: cellLinks[i][j], IsHeader: true)
+                    cellData.append(newItem)
+                }
+                else {
+                    let newItem = TableViewItem(LabelName: cellTitles[i][j], Link: cellLinks[i][j])
+                    cellData.append(newItem)
+                }
+            }
         }
+        
+        
         super.init(coder: aDecoder)
         
     }
@@ -31,43 +124,8 @@ class WebViewController: UIViewController, UITableViewDataSource, UITableViewDel
     
     //Variables
     let cellIdentifier = "cellIdentifier"
-    let cellTitles = ["Research",
-        "GALE PowerSearch",
-        "Biography in Context",
-        "Global Issues in Context",
-        "InfoTrac",
-        "LitFinder",
-        "Opposing Viewpoints in Context",
-        "Science in Context",
-        "Student Resource in Context",
-        "Consumer Reports",
-        "CQ Researcher",
-        "EBSCO",
-        "MAS-ULTRA (in school use)",
-        "Health Source",
-        "SIRS",
-        "Teen Health & Wellness",
-        "World Book"]
-    //TODO: Make 2D Array for different sections? allows for easier identification of headers
     
-    let cellLinks = ["",
-    "http://go.galegroup.com/ps/i.do?v=2.1&u=dist214&pg=BasicSearch&it=static&sw=w&p=GPS",
-    "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=BIC1",
-    "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=GIC",
-    "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=STOM",
-    "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=LITFS",
-    "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=OVIC",
-    "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=SCIC",
-    "http://ezproxy.d214.org:2048/login?url=http://infotrac.galegroup.com/itweb/dist214?db=SUIC",
-    "http://ezproxy.d214.org:2048/login?url=http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=crcro",
-    "http://ezproxy.d214.org:2048/login?url=http://library.cqpress.com/cqres/lpext.dll?f=templates&fn=titlepage.htm",
-    "http://ezproxy.d214.org:2048/login?url=http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=ehost",
-    "http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=ehost&defaultdb=ulh",
-    "http://ezproxy.d214.org:2048/login?url=http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=ehost&defaultdb=hxh",
-    "http://ezproxy.d214.org:2048/login?url=http://sks.sirs.com/",
-    "http://ezproxy.d214.org:2048/login?url=http://www.teenhealthandwellness.com/",
-    "http://ezproxy.d214.org:2048/login?url=http://www.worldbookonline.com/?subacct=N2771"]
-    
+    var selectedIndex = NSIndexPath()
     
     // MARK:  UITextFieldDelegate Methods
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -80,23 +138,28 @@ class WebViewController: UIViewController, UITableViewDataSource, UITableViewDel
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("tableCellItem", forIndexPath: indexPath)
-        let label = cell.viewWithTag(777) as! UILabel
-        let item = cellData[indexPath.row]
-        label.text = item.labelNamed
+        let label = cell.textLabel
+        label!.text = cellData[indexPath.row].labelName
+        if cellData[indexPath.row].isHeaderCell {
+            cell.backgroundColor = UIColor.paleGreenColor()
+        }
+        else {
+            label!.font = UIFont(name: (label!.font?.fontName)!, size: 12)
+        }
         return cell
     }
     
     // MARK:  UITableViewDelegate Methods
     //Triggers when cell view is tapped
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        tableView.deselectRowAtIndexPath(selectedIndex, animated: true)
+        selectedIndex = indexPath
         
         let row = indexPath.row
-        print(cellTitles[row])
+        print(cellData[row].labelName)
         
-        //TODO: Implement webview info
-        let websiteURL = NSURL(fileURLWithPath: cellLinks[row])
-        let urlRequest = NSURLRequest(URL: websiteURL)
+        let websiteURL = NSURL(string: cellData[row].link)
+        let urlRequest = NSURLRequest(URL: websiteURL!)
         webView.loadRequest(urlRequest)
     }
     
